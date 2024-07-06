32b6d5bad1d598460e33305b95a87e869eddcd21
Rodrigo Eduardo <rodrigo_e_soares@estudante.sesisenai.org.br>
2024-07-06 00:15:32 -0300

    feat listar todos os produtos

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -5,5 +5,6 @@ const ProductController = require('../controllers/ProductController')
 const productsRoutes = new Router()
 
 productsRoutes.post('/', ProductController.create)
+productsRoutes.get('/', ProductController.listAll)
 
 module.exports = productsRoutes
